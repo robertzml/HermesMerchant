17950e18b8f509df55ba8294a84354dc454acd9b
robertzml <robertzml@126.com>
2020-07-28 20:01:58 +0800

    add HealthController.

---
+++ HermesMerchant/Controllers/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HermesMerchant.Controllers
+{
+    /// <summary>
+    /// 健康查询控制器
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        #region Action
+        [HttpGet("check")]
+        public IActionResult Get()
+        {
+            return Content("ok");
+        }
+        #endregion //Action
+    }
+}
